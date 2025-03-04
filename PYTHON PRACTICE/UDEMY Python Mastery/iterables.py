a8d5c004ba623123aa753c08d37bad4b7825f103
petersudai <81692851+petersudai@users.noreply.github.com>
2025-03-04 09:42:15 +0300

    iterables

---
+++ PYTHON PRACTICE/UDEMY Python Mastery/iterables.py
@@ -0,0 +1,23 @@
+user = {
+    'name': 'Sudai',
+    'age': 105,
+    'can_swim': True
+}
+
+for item in user.items():
+    print(item)
+
+print('\n')
+
+for item in user.keys():
+    print(item)
+    
+print('\n')
+
+for item in user.values():
+    print(item)
+    
+print('\n')
+
+for key, value in user.items():
+    print(key, value)
