5f55eca9875d41f09762af59c86f878b847b6082
petersudai <81692851+petersudai@users.noreply.github.com>
2025-05-19 10:49:44 +0300

    function parameters - positional vs keyword arguments

---
+++ PYTHON PRACTICE/UDEMY Python Mastery/paremeters.py
@@ -0,0 +1,12 @@
+# Parameters
+# default parameter
+def say_hello(name='Batman', emoji=':)'):
+    print(f'hellloooo {name}{emoji}')
+
+#positional arguments
+say_hello('Lynette', ':-)')
+say_hello('Peter', ':/')
+
+#keyword arguments
+say_hello(name='Bibi', emoji=':))')
+say_hello('Timmy')
